99b21364568bd16cdf2b5a3dfe9232313af7d7f2
Alex Damian Negru <alex.negru@agilefreaks.com>
2021-06-02 14:42:24 +0300

    Add UserType to schema

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,18 +2,15 @@
 
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    description 'The query root of this schema'
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: 'An example field added by the generator'
-    def test_field
-      'Hello World!'
+    field :posts, [UserType], null: true
+
+    def posts
+      User.all
     end
   end
 end
