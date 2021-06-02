8de78ada24b561e97a8e076ec609ed7aa4b60376
Alex Damian Negru <alex.negru@agilefreaks.com>
2021-06-02 15:17:01 +0300

    Cache all fields

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,9 +2,9 @@
 
 module Types
   class UserType < Types::BaseObject
-    field :id, ID, null: false
+    field :id, ID, null: false, cache_fragment: true
     field :name, String, null: false, cache_fragment: true
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false, cache_fragment: true
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false, cache_fragment: true
   end
 end
